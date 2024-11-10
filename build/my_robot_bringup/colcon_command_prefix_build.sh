e3f49c1b681bb45aeb9d854cf97d8d633565b6bc
laser606 <markvanoirschot1@gmail.com>
2024-11-10 23:43:26 +0100

    change it again

---
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/marko/Desktop/Semester_2/Semester-2/install/my_robot_description/share/my_robot_description/package.sh"
---
+++ build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marko/Desktop/Semester_2/Semester-2/src/Ros_Robot_simulation/src/my_robot_description
+# Build directory: /home/marko/Desktop/Semester_2/Semester-2/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
